733e7dd0835d39e51edeaf7ebe20ac0c733a1880
Jorge Izquierdo <izqui97@gmail.com>
2011-12-06 13:28:45 +0100

    6º Tutorial

---
+++ 6.Tabla/TableVC.h
@@ -0,0 +1,17 @@
+//
+//  TableVC.h
+//  Tabla
+//
+//  Created by Jorge Izquierdo on 12/6/11.
+//  Copyright (c) 2011 JIzqApps. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TableVC : UITableViewController
+{
+    
+    NSArray *array;
+}
+
+@end
